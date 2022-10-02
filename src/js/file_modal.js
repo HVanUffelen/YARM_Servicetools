7bbb59bda0ad6167959ab6422c23ddb0226a4b52
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-10-02 15:42:24 +0200

    Start package

---
+++ src/js/file_modal.js
@@ -0,0 +1,53 @@
+;(function ($) {
+    var onready = function () {
+        var local_name;
+
+        $(document).on('click', '.editFile', function (e) {
+            e.preventDefault();
+            var name = $(this).attr('data-name');
+            local_name = $(this).attr('data-local_name');
+            var id = $(this).attr('data-id');
+            var form = $(document).find('#editFileForm');
+            form.find('.local_name').val(local_name);
+            form.find('.name').val(name);
+            form.find('.id').val(id);
+            $('#modal').modal('show');
+        });
+
+        $(document).on('click', '#btn-file-update', function (e) {
+            e.preventDefault();
+            var form = $('#editFileForm');
+            form.validate();
+            if (form.valid()) {
+                var formData = form.serializeArray();
+                var id = formData.find(fd => fd.name == 'id').value.trim();
+                var name = formData.find(fd => fd.name == 'name').value.trim();
+                var new_local_name = formData.find(fd => fd.name == 'local_name').value.trim();
+
+                $.ajax({
+                    type: 'POST',
+                    url: '/dlbt/change_file',
+                    data: { id, name, local_name, new_local_name },
+                    success: function (data) {
+                        var file = data[0];
+                        var localFileName = data[1];
+                        var fileExists = data[2];
+                        var row = $(document).find('#tblData-id_' + file.id);
+                        if (file.name == localFileName && fileExists) {
+                            row.remove();
+                        } else {
+                            row.find('.C-name').html(file.name);
+                            row.find('.C-local_name').html(localFileName);
+                        }
+                    },
+                    error: function (error) {
+                        console.error(error);
+                    }
+                });
+
+                $('#modal').modal('hide');
+            }
+        });
+     };
+     $(document).ready(onready);
+}(jQuery));
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yarm/servicetools',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '340658fa2fc3e5fa44c59bb7306d8881fb3e71e0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yarm/servicetools' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '340658fa2fc3e5fa44c59bb7306d8881fb3e71e0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
