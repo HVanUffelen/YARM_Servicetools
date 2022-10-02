bf59a7f73fd87bbe240acbcf8fbb4225da8da896
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-10-02 13:25:38 +0200

    Start package

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 Route::group(['namespace'=>'Yarm\Servicetools\Http\Controllers','prefix'=>'dlbt','middleware'=>['web']], function (){
 
     // Get comments on illustrations
-    Route::get('/commentsOnIllustrations', 'DataCleaningController@commentsOnIllustrationsList')
+    Route::get('/commentsOnIllustrations', 'ServiceToolsDataCleaningController@commentsOnIllustrationsList')
         ->name('comments_on_illustartions');
 });
